922686518cf144cf66e4fac66d679988127601d2
AlainDlcTVOP <alain_dlc@outlook.com>
2020-09-05 20:10:24 +0200

    added server

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,24 @@
 #include <WiFiServer.h>
 #include <WiFiClient.h>
 #include <esp32-hal.h>
-#include <client.h>
-#include <AES256.h>
-#include <RSA.h>
 #include <SHA256.h>
+#include <RSA.h>
+#include <AES256.h>
+#include <server.h>
 
+//#define SSID "YA-LOCAL"
+//#define PASSWORD "utbildning2020"
 #define SSID "comhemCDAF93"
 #define PASSWORD "D0FDE93BD1"
-/* #define SSID "YA-LOCAL"
-#define PASSWORD "utbildning2020" */
 
-static WiFiClient client;
 static WiFiServer server(PORT);
-static uint8_t tx_counter = 0U;
-
-//#define SSID "comhemCDAF93"
-//#define PASSWORD "D0FDE93BD1"
+static WiFiClient client;
+//static bool authorized = false;
+session_t session;
 
 const uint8_t *auth_key;
 uint8_t message[RSA_SIZE + RSA_SIZE + HASH_SIZE] = {};
 uint8_t hash[HASH_SIZE] = {};
-uint8_t save_message[BUFFER_SIZE];
 
 static uint8_t public_key[RSA_SIZE] = {
     0xC3, 0xA5, 0x4E, 0x87, 0xAD, 0xC6, 0xA4, 0x02, 0x11, 0x0B, 0xF2, 0x75, 0xE3, 0xB6, 0x6D, 0xE6,
@@ -37,6 +34,9 @@ static uint8_t public_key_client[RSA_SIZE] = {
     0xDB, 0x44, 0xDD, 0xA4, 0xB7, 0xAB, 0x9D, 0x86, 0x2B, 0xBD, 0xC1, 0xFD, 0x67, 0xC9, 0x0B, 0xAF,
     0x05, 0x76, 0x3E, 0x4E, 0xD3, 0xD1, 0xDF, 0x9B, 0x7A, 0x75, 0x6E, 0x4C, 0x5F, 0x63, 0x63, 0x75};
 
+//create message1(AES - key + hash)
+
+// To print data in Hex
 void print_data(const uint8_t *data, uint8_t size)
 {
     for (uint8_t i = 0; i < size; i++)
@@ -45,16 +45,21 @@ void print_data(const uint8_t *data, uint8_t size)
     }
     Serial.println();
 }
+//create message1(AES - key + hash)
+//Key: 8D 42 CB 3D 6A 6A 86 C5 5D C3 53 7A C3 D9 B3 74 4E D8 2D 4C 49 2E 30 CC 82 F7 12 85 D1 8E 12 EF
 
+void aes_key();
 void setup()
 {
     Serial.begin(9600);
+
+    Serial.println("connecting.......");
     while (!Serial)
     {
         delay(100);
     }
 
-    WiFi.begin(SSID, PASSWORD);
+    //WiFi.begin(SSID, PASSWORD);
 
     while (WL_CONNECTED != WiFi.status())
     {
@@ -67,25 +72,21 @@ void setup()
     pinMode(BUILTIN_LED, OUTPUT);
     server.begin();
 }
+
 void loop()
 {
+    // Read the received data
+
+    // connect the client
     WiFiClient client = server.available();
 
     if (client.connected() && client.available())
     {
-        client.write(save_message, RSA_SIZE + RSA_SIZE + HASH_SIZE);
+        client.read(message, RSA_SIZE + RSA_SIZE + HASH_SIZE);
     }
 }
-
-void dekryypt_aes_key()
-{ //step 1 :ta emot meddelande
-
-    //step 2 :calculate hash for message1.
-    //step 3 : recieve message1.
-    //step 4: calculate hash for message1.
-    //step 5: compare (calculated)hash for message1, with the hash
-    //step 6: decrypt using client private key.
-    //step 7: decrypt using server public key.
+void aes_key()
+{
     uint8_t temp[RSA_SIZE];
     uint8_t temp1[RSA_BLOCK_SIZE];
     uint8_t temp2[RSA_SIZE - RSA_BLOCK_SIZE];
@@ -94,19 +95,17 @@ void dekryypt_aes_key()
     uint8_t temp5[RSA_SIZE + RSA_SIZE];
     uint8_t hash[HASH_SIZE];
 
-    // VÄND ALLTING för dekryptera
     auth_key = aes256_init_key(NULL);
-
-    rsa_private_decrypt(save_message, public_key, private_key, temp);
+    rsa_private_encrypt((uint8_t *)auth_key, AES_KEY_SIZE, public_key, private_key, temp);
     memcpy(temp1, temp, RSA_BLOCK_SIZE);
     memcpy(temp2, temp + RSA_BLOCK_SIZE, RSA_SIZE - RSA_BLOCK_SIZE);
-    rsa_private_decrypt(temp1, public_key_client, private_key, temp);
-    rsa_private_decrypt(temp2, public_key_client, private_key, temp4);
+    rsa_public_encrypt(temp1, RSA_BLOCK_SIZE, public_key_client, temp3);
+    rsa_public_encrypt(temp2, RSA_BLOCK_SIZE, public_key_client, temp4);
     memcpy(temp5, temp3, RSA_SIZE);
     memcpy(temp5 + RSA_SIZE, temp4, RSA_SIZE);
 
     sha256(temp5, RSA_SIZE + RSA_SIZE, hash);
 
-    memcpy(save_message, temp5, RSA_SIZE + RSA_SIZE);
-    memcpy(save_message + RSA_SIZE + RSA_SIZE, hash, HASH_SIZE);
+    memcpy(message, temp5, RSA_SIZE + RSA_SIZE);
+    memcpy(message + RSA_SIZE + RSA_SIZE, hash, HASH_SIZE);
 }
