17c7b93d4c1f70606d2bada4a803277bb987f3a6
AlainDlcTVOP <alain_dlc@outlook.com>
2020-09-14 09:56:46 +0200

    server update

--- ESP32-RSA-Server/src/main.cpp
+++ ESP32-RSA-Server/src/main.cpp
@@ -1,112 +1,155 @@
+#include <RSA.h>
 #include <WiFi.h>
-#include <stdbool.h>
+#include <SHA256.h>
+#include <AES256.h>
 #include <Arduino.h>
 #include <IPAddress.h>
-#include <WiFiServer.h>
 #include <WiFiClient.h>
-#include <esp32-hal.h>
-#include <SHA256.h>
-#include <RSA.h>
-#include <AES256.h>
+#include <WiFiServer.h>
 #include <server.h>
+#include <esp32-hal.h>
 
-//#define SSID "YA-LOCAL"
-//#define PASSWORD "utbildning2020"
-#define SSID "comhemCDAF93"
-#define PASSWORD "D0FDE93BD1"
+#define SSID "YA-LOCAL"
+#define PASSWORD "utbildning-2020"
 
-static WiFiServer server(PORT);
+#define PORT (12345U)
+#define BUFSIZE (3 * RSA_SIZE)
+
+// PUBLIC_DECRYPT(PRIVATE_DECRYPT(REQ_ID | DATA)) | HASH => AUTH | AES KEY
+// AES_DECRYPT(REQ_ID | SESSION_ID) | HASH => TEMPERATURE | SESSION_ID
+
+// PUBLIC_DECRYPT(PRIVATE_DECRYPT(RES_STATUS | SESSION_ID)) | HASH => OKAY | SESSION_ID
+// AES_DECRYPT(RES_STATUS | DATA) | HASH => OKAY | temperature
+
+static uint8_t client_public_key[RSA_SIZE] = {
+    0xD1, 0x13, 0x2B, 0x14, 0x8E, 0xA4, 0x70, 0x89, 0xA0, 0x3E, 0x3B, 0x2E, 0x3F, 0xDD, 0xDC, 0xC0,
+    0xBD, 0x88, 0x49, 0x2B, 0xFC, 0x04, 0x6C, 0xB9, 0x23, 0x5C, 0x1F, 0x5B, 0x68, 0x6F, 0x00, 0xDD,
+    0xF0, 0x8A, 0x9A, 0x12, 0x7C, 0x64, 0x33, 0x8F, 0x6B, 0xF4, 0xC5, 0x62, 0x00, 0x68, 0x20, 0xDE,
+    0xD3, 0xA2, 0xA2, 0xEA, 0xD6, 0x04, 0x9C, 0x15, 0xB9, 0x23, 0xC3, 0x8E, 0x02, 0xEC, 0x7E, 0x7B};
+static uint8_t server_public_key[RSA_SIZE] = {
+    0x92, 0xF4, 0x13, 0xDD, 0x91, 0xFE, 0x15, 0xD3, 0xCA, 0x3D, 0xD8, 0x65, 0x32, 0x8D, 0xC7, 0x64,
+    0xA5, 0x7F, 0xF9, 0x4C, 0xE2, 0x9B, 0x03, 0x96, 0xF5, 0xB7, 0x80, 0x55, 0xDE, 0xB0, 0xEA, 0x58,
+    0x97, 0x7F, 0x9F, 0x25, 0x8C, 0x45, 0x5A, 0xF4, 0x50, 0x21, 0xF8, 0x95, 0xCC, 0xB8, 0x53, 0xE4,
+    0x8C, 0x43, 0xD0, 0x87, 0x08, 0x97, 0xF3, 0x31, 0x39, 0x50, 0x17, 0x5F, 0xE1, 0xCD, 0xC7, 0xD5};
+static uint8_t server_private_key[RSA_SIZE] = {
+    0x29, 0xD2, 0xC5, 0x84, 0x9D, 0xF4, 0x4E, 0x8A, 0x04, 0x59, 0x2D, 0xA9, 0x3F, 0x86, 0x12, 0x65,
+    0x96, 0xA4, 0xA1, 0x73, 0x3C, 0x5B, 0x19, 0xDE, 0x70, 0xF8, 0x54, 0xD2, 0x1B, 0x9B, 0x06, 0x56,
+    0xD7, 0x67, 0x93, 0x1A, 0x0B, 0x95, 0x06, 0xD1, 0xF2, 0x04, 0x64, 0xCB, 0x13, 0xDA, 0x9E, 0x25,
+    0x18, 0xCA, 0x79, 0xAB, 0x29, 0xCF, 0xE4, 0x02, 0xBA, 0x3E, 0x3B, 0xDC, 0x53, 0x84, 0x43, 0x51};
+
 static WiFiClient client;
-//static bool authorized = false;
-session_t session;
+static WiFiServer server(PORT);
+static uint32_t session_id = 0U;
 
-const uint8_t *auth_key;
-uint8_t message[RSA_SIZE + RSA_SIZE + HASH_SIZE] = {};
-uint8_t hash[HASH_SIZE] = {};
-
-static uint8_t public_key[RSA_SIZE] = {
-    0xC3, 0xA5, 0x4E, 0x87, 0xAD, 0xC6, 0xA4, 0x02, 0x11, 0x0B, 0xF2, 0x75, 0xE3, 0xB6, 0x6D, 0xE6,
-    0x55, 0xA0, 0x17, 0x60, 0x16, 0xC2, 0x12, 0x58, 0xA9, 0xC6, 0xF5, 0x91, 0xCD, 0xB7, 0xA7, 0xA9};
-static uint8_t private_key[RSA_SIZE] = {
-    0x56, 0x29, 0x30, 0xE2, 0x73, 0xD7, 0x6D, 0x57, 0x33, 0xA6, 0xAD, 0x4A, 0xD9, 0xD3, 0xF7, 0xA5,
-    0x98, 0xF3, 0xFA, 0x07, 0x64, 0x7D, 0xE5, 0xE4, 0x4B, 0x13, 0x5C, 0x90, 0x38, 0xF4, 0x3B, 0x59};
-static uint8_t public_key_client[RSA_SIZE] = {
-    0xDB, 0x44, 0xDD, 0xA4, 0xB7, 0xAB, 0x9D, 0x86, 0x2B, 0xBD, 0xC1, 0xFD, 0x67, 0xC9, 0x0B, 0xAF,
-    0x05, 0x76, 0x3E, 0x4E, 0xD3, 0xD1, 0xDF, 0x9B, 0x7A, 0x75, 0x6E, 0x4C, 0x5F, 0x63, 0x63, 0x75};
-
-//create message1(AES - key + hash)
-
-// To print data in Hex
-void print_data(const uint8_t *data, uint8_t size)
+static void send_response(response_t *res)
 {
-    for (uint8_t i = 0; i < size; i++)
-    {
-        Serial.printf("%02X ", data[i]);
-    }
-    Serial.println();
+    sha256(res->data, res->length, res->data + res->length);
+    res->length += HASH_SIZE;
+    client.write(res->data, res->length);
+    client.flush();
 }
-//create message1(AES - key + hash)
-//Key: 8D 42 CB 3D 6A 6A 86 C5 5D C3 53 7A C3 D9 B3 74 4E D8 2D 4C 49 2E 30 CC 82 F7 12 85 D1 8E 12 EF
 
-void aes_key();
 void setup()
 {
     Serial.begin(9600);
+    delay(3000);
 
-    Serial.println("connecting.......");
-    while (!Serial)
-    {
-        delay(100);
-    }
-
-    //WiFi.begin(SSID, PASSWORD);
-
+    WiFi.begin(SSID, PASSWORD);
     while (WL_CONNECTED != WiFi.status())
     {
-        delay(3000);
-        WiFi.begin(SSID, PASSWORD);
         Serial.print(".");
+        delay(1000);
     }
+
     Serial.print("\nIP Address: ");
     Serial.println(WiFi.localIP());
-    pinMode(BUILTIN_LED, OUTPUT);
+
     server.begin();
 }
 
 void loop()
 {
-    // Read the received data
+    client = server.available();
 
-    // connect the client
-    WiFiClient client = server.available();
-
-    if (client.connected() && client.available())
+    if (client && client.connected())
     {
-        client.read(message, RSA_SIZE + RSA_SIZE + HASH_SIZE);
+        // Wait on receiving data from the client
+        while (!client.available())
+        {
+            delay(1);
+        }
+
+        // Read the received data
+        uint8_t buffer[BUFSIZE] = {};
+        uint8_t length = client.read(buffer, BUFSIZE);
+
+        response_t response = {};
+
+        if ((length != 2 * RSA_SIZE + HASH_SIZE) && (length != AES_CIPHER_SIZE + HASH_SIZE))
+        {
+            response.length = 1U;
+            response.data[0] = BAD_REQUEST;
+        }
+        else
+        {
+            length -= HASH_SIZE;
+            uint8_t hash[HASH_SIZE] = {};
+            sha256(buffer, length, hash);
+            if (memcmp(hash, buffer + length, HASH_SIZE))
+            {
+                response.length = 1U;
+                response.data[0] = HASH_ERROR;
+            }
+            else if (length == 2 * RSA_SIZE)
+            {
+                // RSA
+                // Authentication (AUTH)
+                uint8_t temp[RSA_SIZE] = {};
+                uint8_t len = rsa_private_decrypt(buffer, server_public_key, server_private_key, temp);
+                len += rsa_private_decrypt(buffer + RSA_SIZE, server_public_key, server_private_key, temp + len);
+                if (len != RSA_SIZE)
+                {
+                    response.length = 1U;
+                    response.data[0] = BAD_REQUEST;
+                }
+                else
+                {
+                    len = rsa_public_decrypt(temp, client_public_key, temp);
+                    if (len != AES_KEY_SIZE + 1)
+                    {
+                        response.length = 1U;
+                        response.data[0] = BAD_REQUEST;
+                    }
+                    else if (temp[0] != AUTH)
+                    {
+                        response.length = 1U;
+                        response.data[0] = BAD_REQUEST;
+                    }
+                    else
+                    {
+                        response.length = 1U;
+                        response.data[0] = OKAY;
+                        aes256_init_key(temp + 1);
+                        randomSeed(micros());
+                        session_id = random(1U, 0xFFFFFFF);
+                        for (uint8_t i = 0U; i < sizeof(session_id); i++)
+                        {
+                            response.data[response.length] = (uint8_t)(session_id >> (i * 8));
+                            response.length++;
+                        }
+
+                        // Encrypt the data using the private key of the server
+
+                        // Encrypt the result of the previous step using the public key of the client
+                    }
+                }
+            }
+            else
+            {
+                // AES
+            }
+        }
+
+        send_response(&response);
     }
-}
-void aes_key()
-{
-    uint8_t temp[RSA_SIZE];
-    uint8_t temp1[RSA_BLOCK_SIZE];
-    uint8_t temp2[RSA_SIZE - RSA_BLOCK_SIZE];
-    uint8_t temp3[RSA_SIZE];
-    uint8_t temp4[RSA_SIZE];
-    uint8_t temp5[RSA_SIZE + RSA_SIZE];
-    uint8_t hash[HASH_SIZE];
-
-    auth_key = aes256_init_key(NULL);
-    rsa_private_encrypt((uint8_t *)auth_key, AES_KEY_SIZE, public_key, private_key, temp);
-    memcpy(temp1, temp, RSA_BLOCK_SIZE);
-    memcpy(temp2, temp + RSA_BLOCK_SIZE, RSA_SIZE - RSA_BLOCK_SIZE);
-    rsa_public_encrypt(temp1, RSA_BLOCK_SIZE, public_key_client, temp3);
-    rsa_public_encrypt(temp2, RSA_BLOCK_SIZE, public_key_client, temp4);
-    memcpy(temp5, temp3, RSA_SIZE);
-    memcpy(temp5 + RSA_SIZE, temp4, RSA_SIZE);
-
-    sha256(temp5, RSA_SIZE + RSA_SIZE, hash);
-
-    memcpy(message, temp5, RSA_SIZE + RSA_SIZE);
-    memcpy(message + RSA_SIZE + RSA_SIZE, hash, HASH_SIZE);
-}
 }
--- ESP32-RSA-Server/src/server.h
+++ ESP32-RSA-Server/src/server.h
@@ -1,44 +1,35 @@
 #ifndef SERVER_H
 #define SERVER_H
-
-#define PORT (12345U)
-#define BUFFER_SIZE (256U)
-
-#define SESSION_PERIOD (60000U)
-#define AUTH_MES_SIZE (84U)
-#define REQ_MES_SIZE (36U)
-#define SESSION_ID (3U)
-#include <SHA256.h>
 #include <RSA.h>
+#include <SHA256.h>
 #include <AES256.h>
-enum sending_types
+#define BUFSIZE (3 * RSA_SIZE)
+enum Request
 {
-    LED_ON = 1,
-    LED_OFF,
-    LED_STATUS,
+    AUTH,
+    CLOSE,
     TEMPERATURE,
-    END_SESSION
+    TURN_LED_ON,
+    TURN_LED_OFF
 };
 
-enum receiving_types
+enum Status
 {
-    REQUEST_DONE = 0,
-    ERROR
+    OKAY,
+    UNAUTH,
+    EXPIRED,
+    TIMEOUT,
+    WAITING,
+    HASH_ERROR,
+    BAD_REQUEST,
+    DISCONNECTED,
+    UNKNOWN_ERROR
 };
 
 typedef struct
 {
-    uint8_t the_secret[HASH_SIZE] = {};
-    uint8_t session_Id[SESSION_ID] = {};
-    uint8_t request[AES_BLOCK_SIZE - SESSION_ID] = {};
-} message_info;
-
-typedef struct
-{
-    uint8_t session_Id[SESSION_ID] = {};
-    uint32_t end_session;
-} session_t;
-
-message_info message_decrypting(uint8_t mes_len, uint8_t *message);
+    uint8_t data[BUFSIZE];
+    size_t length;
+} response_t;
 
 #endif /* SERVER_H */
